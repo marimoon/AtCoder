438db9f755a710a91c9cc5eedec10f1df3a3316d
marimo <43314030+marimoon@users.noreply.github.com>
2021-01-20 23:53:07 +0900

    Add files via upload

---
+++ ABC155_A/main.py
@@ -0,0 +1,22 @@
+﻿def get_list(type=int):
+    return list(map(type, input().split()))
+
+def get_int():
+    return int(input())
+
+def get_str():
+    return input()
+
+
+def main():
+    A,B,C=list(map(int, input().split()))
+    if (A==B) and (not A == C):
+      print("Yes")
+    elif (B==C) and (not B == A):
+      print("Yes")
+    elif (C==A) and (not C == B):
+      print("Yes")
+    else:
+      print("No")
+if __name__ == "__main__":
+    main()
