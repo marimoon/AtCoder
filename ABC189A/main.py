e8295f978cec49dea091e44619327bb6ec23c2e0
marimo <43314030+marimoon@users.noreply.github.com>
2021-01-24 00:15:37 +0900

    Add files via upload

---
+++ ABC189A/main.py
@@ -0,0 +1,19 @@
+﻿def get_list(type=int):
+    return list(map(type, input().split()))
+
+def get_int():
+    return int(input())
+
+def get_str():
+    return input()
+
+
+def main():
+    C = input()
+    if (C[0] == C[1]) and (C[1] == C[2]):
+        print("Won")
+    else:
+        print("Lost")
+
+if __name__ == "__main__":
+    main()
