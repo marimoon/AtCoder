41bdca51ea17782c3e7df8e8c83c5eea41b57650
marimo <43314030+marimoon@users.noreply.github.com>
2021-01-25 00:08:40 +0900

    Add files via upload

---
+++ ABC189B/main.py
@@ -0,0 +1,22 @@
+﻿def get_list(type=int):
+    return list(map(type, input().split()))
+
+def get_int():
+    return int(input())
+
+def get_str():
+    return input()
+
+def main():
+    N, X = get_list(int)
+    amount = 0
+    for i in range(N):
+        v,p = get_list(int)
+        amount += v*p
+        if amount > X*100:
+            print(i+1)
+            exit()
+    print("-1")
+
+if __name__ == "__main__":
+    main()
