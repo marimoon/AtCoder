1b85ce2738717415f940a4da59077faa6b306e06
marimo <43314030+marimoon@users.noreply.github.com>
2021-01-30 23:14:40 +0900

    Add files via upload

---
+++ ABC190_A/main.py
@@ -0,0 +1,25 @@
+﻿def get_list(type=int):
+    return list(map(type, input().split()))
+
+def get_int():
+    return int(input())
+
+def get_str():
+    return input()
+
+
+def main():
+    A, B, C = get_list(int)
+    if C==0:
+        if A > B:
+            print("Takahashi")
+        else:
+            print("Aoki")
+    else:
+        if A >= B:
+            print("Takahashi")
+        else:
+            print("Aoki")
+
+if __name__ == "__main__":
+    main()
