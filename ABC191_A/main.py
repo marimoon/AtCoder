8fda3ff4fac11d117e0571a3855845f9c8ce0f01
marimo <43314030+marimoon@users.noreply.github.com>
2021-02-06 22:56:57 +0900

    Add files via upload

---
+++ ABC191_A/main.py
@@ -0,0 +1,21 @@
+﻿def get_list(type=int):
+    return list(map(type, input().split()))
+
+def get_int():
+    return int(input())
+
+def get_str():
+    return input()
+
+
+def main():
+    V, T, S, D = get_list(int)
+    x = V*T
+    y = V*S
+    if (x <= D) and (D <= y):
+        print("No")
+    else:
+        print("Yes")
+
+if __name__ == "__main__":
+    main()
