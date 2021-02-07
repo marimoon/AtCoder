3b606059da5b3f652ee1c85121d6666dce387890
marimo <43314030+marimoon@users.noreply.github.com>
2021-02-07 17:05:10 +0900

    Add files via upload

---
+++ ABC191_B/main.py
@@ -0,0 +1,17 @@
+﻿def get_list(type=int):
+    return list(map(type, input().split()))
+
+def get_int():
+    return int(input())
+
+def get_str():
+    return input()
+
+
+def main():
+    N, X = get_list(int)
+    A =  get_list(int)
+    print(" ".join([ str(val) for val in A if val != X ]) )
+
+if __name__ == "__main__":
+    main()
